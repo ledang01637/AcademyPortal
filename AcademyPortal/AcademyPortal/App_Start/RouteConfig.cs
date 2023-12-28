ba87a640799346c63d6959207349cc19852125ec
ledang01637 <147400284+ledang01637@users.noreply.github.com>
2023-12-28 23:28:03 +0700

    Lấy dữ liệu từ input

--- AcademyPortal/AcademyPortal/App_Start/RouteConfig.cs
+++ AcademyPortal/AcademyPortal/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AcademyPortal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Permission", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AcademyPortal/AcademyPortal/Controllers/PermissionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcademyPortal.Handler;
+
+namespace AcademyPortal.Controllers
+{
+    public class PermissionController : Controller
+    {
+        public ActionResult Index(string UserName)
+        {
+            List<Item> ListItem = new List<Item>();
+            Item item = new Item();
+            Item item2 = new Item();
+            Item item3 = new Item();
+            item.UserName = UserName;
+            item2.UserName = "c";
+            item3.UserName = "d";
+            ListItem.Add(item);
+            ListItem.Add(item2);
+            ListItem.Add(item3);
+            return View(ListItem);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SavePermission()
+        {
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
